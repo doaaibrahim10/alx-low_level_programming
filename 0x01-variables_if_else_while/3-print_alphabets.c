ad660e4836393efdbb86391d0fa3199b60ff52c3
doaa <idoaa8788@gmail.com>
2023-04-21 15:03:50 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	char d;
+
+	ch = 'a';
+	d = 'A';
+	while
+		(ch <= 'z') {
+			putchar(ch);
+			ch++;
+		}
+	while
+		(d <= 'Z') {
+			putchar (d);
+			d++;
+		}
+	putchar('\n');
+	return (0);
+}
